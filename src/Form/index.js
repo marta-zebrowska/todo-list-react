791c53ba17bdd32f3c512f3dde36ab67f0111784
Marta Zebrowska-Giers <zeberita@gmail.com>
2022-12-14 11:26:20 +0100

    task list without interactions yet

---
+++ src/Form/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const Form =  () => (
+    <form className="form">
+            <input
+              className="form__newTask"
+              placeholder="Co jest do zrobienia?"
+            />
+            <button className="form__button">Dodaj zadanie</button>
+          </form>
+);
+
+export default Form;
