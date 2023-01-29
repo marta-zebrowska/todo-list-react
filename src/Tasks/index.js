3d6e12a4139ab29ff91fa85f766af52e9605237e
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-01-29 21:02:55 +0100

    destructuring

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -2,21 +2,21 @@ import "./style.css";
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <ul className="tasks">
-    {tasks.map(task => (
+    {tasks.map(({ id, content, done }) => (
       <li
-      className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""}`} key={task.id}>        
+      className={`tasks__item${done && hideDone ? " tasks__item--hidden" : ""}`} key={id}>        
         <button 
         className="tasks__button tasks__button--done"
-        onClick={() => toggleTaskDone(task.id)}
+        onClick={() => toggleTaskDone(id)}
         >
-             {task.done ? "✔" : ""}
+             {done ? "✔" : ""}
             </button>
-            <span className={`tasks${task.done ? " tasks__content--done" : ""}`}>
-             {task.content}
+            <span className={`tasks${done ? " tasks__content--done" : ""}`}>
+             {content}
             </span>
             <button 
             className="tasks__button tasks__button--remove"
-            onClick={() => removeTask(task.id)}
+            onClick={() => removeTask(id)}
             >
             🗑 
             </button>
