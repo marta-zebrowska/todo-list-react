5ceae12edb19ddde6e541a872302aa7bec1c6cb3
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-07 11:44:49 +0100

    styled-components added to Section

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,15 +1,15 @@
-import "./style.css";
+import { StyledSection, SectionBody, Header, Title } from "./styled";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-    <section className="section">
-    <header className="section__header">
-      <h2 className="section__title">{title}</h2>
+    <StyledSection>
+    <Header>
+      <Title>{title}</Title>
       {extraHeaderContent}
-    </header>
-    <div className="section__body">
+    </Header>
+    <SectionBody>
       {body}
-    </div>
-  </section>
+    </SectionBody>
+  </StyledSection>
 );
 
 export default Section;
---
+++ src/Section/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+    margin: 10px 0;
+    background-color: white;
+    box-shadow: 0 0 5px #ddd;
+`;
+
+export const SectionBody = styled.div`
+ padding: 20px;
+`;
+
+export const Header = styled.header`
+    border-bottom: 1px solid #ddd;
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 20px;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 20px;
+    padding: 20px;
+
+@media (max-width: 767px) {
+        grid-template-columns: 1fr;
+    }
+`;
+
+export const Title = styled.h2`
+    font-size: 20px;
+    margin: 0;
+`;
