2a8e1d97fce5e3f55f9c2f49da9bdb9dbd3fe43d
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-08 14:17:46 +0100

    theme added

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,38 +1,37 @@
 import styled from "styled-components";
 
 export const StyledForm = styled.form`
-    display: grid;
-    grid-template-columns: 1fr auto;
-    grid-gap: 20px;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: 20px;
 
-@media (max-width: 767px) {
-        grid-template-columns: 1fr;
-        display: flex;
-        flex-wrap: wrap;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    grid-template-columns: 1fr;
+    display: flex;
+    flex-wrap: wrap;
+  }
 `;
 
 export const StyledInput = styled.input`
-    border: 2px solid #ddd;
-    padding: 10px;
-    width: 100%;
+  border: 2px solid ${({ theme }) => theme.color.gallery};
+  padding: 10px;
+  width: 100%;
 `;
 
 export const Button = styled.button`
-    padding: 10px;
-    background: teal;
-    color: white;
-    border: none;
-    cursor: pointer;
-    transition: 1s;
+  padding: 10px;
+  background-color: ${({ theme }) => theme.color.teal};
+  color: ${({ theme }) => theme.color.white};
+  border: none;
+  cursor: pointer;
+  transition: 1s;
 
-&:hover {
-    background: hsl(180, 100%, 35%);
+  &:hover {
+    filter: brightness(130%);
     transform: scale(1.2);
-    ;
-}
+  }
 
-@media (max-width: 767px) {
-        width: 100%;
-    }
-    `;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 100%;
+  }
+`;
