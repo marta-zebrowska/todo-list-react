c46d024f314582b42468f8a2872ca5bc438e001e
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-08 14:16:31 +0100

    theme created

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  color: {
+    gallery: "#eee",
+    white: "#fff",
+    teal: "#008080",
+    parsley: "#18671F",
+    crimson: "#db143c",
+  },
+  breakpoint: {
+    mobile: 767,
+  },
+};
