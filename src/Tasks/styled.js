1ef2002749b3be961b5611e329e26d71015c9950
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-08 14:18:56 +0100

    theme added

--- src/Tasks/styled.js
+++ src/Tasks/styled.js
@@ -1,61 +1,69 @@
 import styled, { css } from "styled-components";
- 
+
 export const TasksList = styled.ul`
-    margin: 0;
-    list-style: none;
-    padding: 0;
+  margin: 0;
+  list-style: none;
+  padding: 0;
 `;
 
 export const Item = styled.li`
-    display: grid;
-    grid-template-columns: auto 1fr auto;
-    grid-gap: 10px;
-    border-bottom: 1px solid #ddd;
-    align-items: center;
-    padding: 10px;
-    padding-left: 0;
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-gap: 10px;
+  border-bottom: 1px solid ${({ theme }) => theme.color.gallery};
+  align-items: center;
+  padding: 10px;
+  padding-left: 0;
 
-${({ hidden }) => hidden && css`
-   display: none;
- `}
+  ${({ hidden }) =>
+    hidden &&
+    css`
+      display: none;
+    `}
 `;
 
 export const Content = styled.span`
-${({ done }) => done && css`
-text-decoration: line-through;
-`}
+  ${({ done }) =>
+    done &&
+    css`
+      text-decoration: line-through;
+    `}
 `;
 
 export const Button = styled.button`
-    color: white;
-    width: 30px;
-    height: 30px;
-    border: none;
-    cursor: pointer;
+  background-color: ${({ theme }) => theme.color.crimson};
+  width: 30px;
+  height: 30px;
+  border: none;
+  cursor: pointer;
 
-    ${({ toggleDone }) => toggleDone && css`
-      background: #1a7f1a;
+  ${({ toggleDone }) =>
+    toggleDone &&
+    css`
+      background-color: ${({ theme }) => theme.color.parsley};
       transition: 0.4s;
 
       &:hover {
-    background-color: hsl(120, 100%, 35%);
-        }
-        
-    &:active {
-        background-color: hsl(120, 100%, 45%);
-         }
-      `}
+        filter: brightness(120%);
+      }
 
-${({ remove }) => remove && css`
-    background-color: hsl(0, 0%, 80%);
-    transition: 1s;
+      &:active {
+        filter: brightness(140%);
+      }
+    `}
 
-    &:hover {
-    background-color: hsl(347, 93%, 60%);
-}
+  ${({ remove }) =>
+    remove &&
+    css`
+      color: ${({ theme }) => theme.color.white};
+      transition: 1s;
 
-&:active {
-    background-color: hsl(347, 93%, 70%);
-}
-`}
+      &:hover {
+        filter: brightness(120%);
+      }
+
+      &:active {
+        filter: brightness(140%);
+      }
+    `}
 `;
