a1b0621a714f4118944679c44fb4f83baee6bc53
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-02-10 12:48:27 +0100

    empty line added

--- src/useTasks.js
+++ src/useTasks.js
@@ -5,6 +5,7 @@ export const useTasks = () => {
   const [tasks, setTasks] = useState(
     JSON.parse(localStorage.getItem("tasks")) || []
   );
+  
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
