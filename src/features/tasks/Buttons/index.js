9ea8647d5916ff7b0ee4ddc149712349d236b593
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-03-01 13:41:20 +0100

    redux added to tasks

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,20 +1,26 @@
+import { useSelector, useDispatch } from "react-redux";
+import { selectTasks, toggleHideDone } from "../tasksSlice";
 import { Wrapper, StyledButton } from "./styled";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-  <Wrapper>
-    {tasks.length > 0 && (
-      <>
-        <StyledButton onClick={toggleHideDone}>
-          {hideDone ? "Pokaż " : "Ukryj "} ukończone
-        </StyledButton>
-        <StyledButton
-          onClick={setAllDone}
-          disabled={tasks.every(({ done }) => done)}
-        >
-          Ukończ wszystkie
-        </StyledButton>
-      </>
-    )}
-  </Wrapper>
-);
+const Buttons = ({ setAllDone }) => {
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+  return (
+    <Wrapper>
+      {tasks.length > 0 && (
+        <>
+          <StyledButton onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? "Pokaż " : "Ukryj "} ukończone
+          </StyledButton>
+          <StyledButton
+            onClick={setAllDone}
+            disabled={tasks.every(({ done }) => done)}
+          >
+            Ukończ wszystkie
+          </StyledButton>
+        </>
+      )}
+    </Wrapper>
+  )
+};
 export default Buttons;
