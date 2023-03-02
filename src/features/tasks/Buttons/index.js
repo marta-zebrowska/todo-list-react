4bfa2f800942e46b02c7088c5106427e1350b7cd
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-03-02 11:09:00 +0100

    setAllDone moved to Redux

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,8 +1,8 @@
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleHideDone } from "../tasksSlice";
+import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
 import { Wrapper, StyledButton } from "./styled";
 
-const Buttons = ({ setAllDone }) => {
+const Buttons = () => {
   const { tasks, hideDone } = useSelector(selectTasks);
   const dispatch = useDispatch();
   return (
@@ -13,7 +13,7 @@ const Buttons = ({ setAllDone }) => {
             {hideDone ? "Pokaż " : "Ukryj "} ukończone
           </StyledButton>
           <StyledButton
-            onClick={setAllDone}
+            onClick={() => dispatch(setAllDone())}
             disabled={tasks.every(({ done }) => done)}
           >
             Ukończ wszystkie
@@ -21,6 +21,6 @@ const Buttons = ({ setAllDone }) => {
         </>
       )}
     </Wrapper>
-  )
+  );
 };
 export default Buttons;
