9ecd049cef11a572d005da7ca861c43ab7672d00
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-03-08 12:28:59 +0100

    FetchExampleTasksSuccess added

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,11 +1,11 @@
 import { takeEvery, call, put } from "redux-saga/effects";
 import { getExampleTasks } from "./getExampleTasks";
-import { fetchExampleTasks, setTasks } from "./tasksSlice";
+import { fetchExampleTasks, fetchExampleTasksSuccess } from "./tasksSlice";
 
 function* fetchExampleTasksHandler() {
   try {
     const exampleTasks = yield call(getExampleTasks);
-    yield put(setTasks(exampleTasks));
+    yield put(fetchExampleTasksSuccess(exampleTasks));
   } catch (error) {
     yield call(alert, "Coś poszło nie tak!");
   }
