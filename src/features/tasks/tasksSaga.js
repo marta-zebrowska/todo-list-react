8c99f95c6783d7cf19c1081a7fa5c1fb048ba012
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-03-08 11:40:29 +0100

    tasksSaga created

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,17 @@
+import { takeEvery, call, put } from "redux-saga/effects";
+import { getExampleTasks } from "./getExampleTasks";
+import { fetchExampleTasks, setTasks } from "./tasksSlice";
+
+function* fetchExampleTasksHandler() {
+  try {
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(setTasks(exampleTasks));
+  } catch (error) {
+    yield call(alert, "Coś poszło nie tak!");
+  }
+}
+
+export function* watchFetchExampleTasks() {
+  console.log("Saga jest podłączona!");
+  yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
+}
