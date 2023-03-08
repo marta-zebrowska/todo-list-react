1511309d1fbd6bd175a7dc7b144b37094e388ca9
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-03-08 11:39:54 +0100

    new button created

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,5 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasksState, selectIsEveryTaskDone, toggleHideDone, setAllDone } from "../tasksSlice";
+import { Button } from "../Form/styled";
+import { 
+  selectTasksState, 
+  selectIsEveryTaskDone, 
+  toggleHideDone, 
+  setAllDone, 
+  fetchExampleTasks 
+} from "../tasksSlice";
 import { Wrapper, StyledButton } from "./styled";
 
 const Buttons = () => {
@@ -8,6 +15,9 @@ const Buttons = () => {
   const dispatch = useDispatch();
   return (
     <Wrapper>
+      <Button onClick={() => dispatch(fetchExampleTasks())}>
+        Pobierz przykładowe zadania
+      </Button>
       {tasks.length > 0 && (
         <>
           <StyledButton onClick={() => dispatch(toggleHideDone())}>
