4d64af602298c8e0647832c0f0350d1487f35ee0
Marta Zebrowska-Giers <zeberita@gmail.com>
2023-03-09 10:09:50 +0100

    delay effect added to button

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,12 +1,14 @@
-import { takeEvery, call, put } from "redux-saga/effects";
+import { takeEvery, call, put, delay } from "redux-saga/effects";
 import { getExampleTasks } from "./getExampleTasks";
-import { fetchExampleTasks, fetchExampleTasksSuccess } from "./tasksSlice";
+import { fetchExampleTasks, fetchExampleTasksError, fetchExampleTasksSuccess } from "./tasksSlice";
 
 function* fetchExampleTasksHandler() {
   try {
+    yield delay(1000);
     const exampleTasks = yield call(getExampleTasks);
     yield put(fetchExampleTasksSuccess(exampleTasks));
   } catch (error) {
+    yield put(fetchExampleTasksError());
     yield call(alert, "Coś poszło nie tak!");
   }
 }
